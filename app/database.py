5882201d80a08503a36d88d5fc2bdd1106c62f85
Edem Ziddah <contact@codevoweb.com>
2022-11-10 20:12:47 +0000

    updated

--- app/database.py
+++ app/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+SQLITE_DATABASE_URL = "sqlite:///./note.db"
+
 engine = create_engine(
-    "sqlite:///./note.db", echo=True, connect_args={"check_same_thread": False}
+    SQLITE_DATABASE_URL, echo=True, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- app/models.py
+++ app/models.py
@@ -1,16 +1,17 @@
 from .database import Base
 from sqlalchemy import TIMESTAMP, Column, String, Boolean
 from sqlalchemy.sql import func
-from fastapi_utils.guid_type import GUID, GUID_DEFAULT_SQLITE
+from fastapi_utils.guid_type import GUID, GUID_SERVER_DEFAULT_POSTGRESQL
 
 
 class Note(Base):
     __tablename__ = 'notes'
-    id = Column(GUID, primary_key=True, default=GUID_DEFAULT_SQLITE)
+    id = Column(GUID, primary_key=True,
+                server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     category = Column(String, nullable=True)
-    published = Column(Boolean, nullable=False, default=True)
+    published = Column(Boolean, nullable=False, server_default='True')
     createdAt = Column(TIMESTAMP(timezone=True),
                        nullable=False, server_default=func.now())
     updatedAt = Column(TIMESTAMP(timezone=True),
