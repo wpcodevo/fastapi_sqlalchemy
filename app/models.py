fcce5384dcb7702b509018a94525ac8e715c73c2
Edem Ziddah <contact@codevoweb.com>
2022-11-11 08:38:46 +0000

    updated

--- app/models.py
+++ app/models.py
@@ -1,17 +1,16 @@
 from .database import Base
 from sqlalchemy import TIMESTAMP, Column, String, Boolean
 from sqlalchemy.sql import func
-from fastapi_utils.guid_type import GUID, GUID_SERVER_DEFAULT_POSTGRESQL
+from fastapi_utils.guid_type import GUID, GUID_DEFAULT_SQLITE
 
 
 class Note(Base):
     __tablename__ = 'notes'
-    id = Column(GUID, primary_key=True,
-                server_default=GUID_SERVER_DEFAULT_POSTGRESQL)
+    id = Column(GUID, primary_key=True, default=GUID_DEFAULT_SQLITE)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     category = Column(String, nullable=True)
-    published = Column(Boolean, nullable=False, server_default='True')
+    published = Column(Boolean, nullable=False, default=True)
     createdAt = Column(TIMESTAMP(timezone=True),
                        nullable=False, server_default=func.now())
     updatedAt = Column(TIMESTAMP(timezone=True),
